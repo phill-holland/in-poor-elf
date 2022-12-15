6e9b2b09fc366e264e548882af62611e2c060f7e
Phill Holland <phill.holland@and.digital>
2022-12-15 15:45:14 +0000

    added elf3

--- main.rs
+++ main.rs
@@ -41,6 +41,147 @@ fn in_poor_elf1()
     println!("Total {}", total);
 }
 
+fn in_poor_elf3()
+{
+    let mut sum:u32 = 0;
+
+    if let Ok(lines) = read_lines("elf3.txt")
+    {
+        for line in lines 
+        {
+            if let Ok(ip) = line
+            {
+             
+                let mut len = ip.len();
+                if len % 2 == 0 
+                {
+                    len /= 2;
+                    
+                    let mut a:Vec<u32> = Vec::new();
+                    let mut b:Vec<u32> = Vec::new();
+
+                    let mut counter = 0;
+                    
+                    for c in ip.chars()
+                    {                        
+                        let mut v = c as u32; 
+
+                        if v >= 97 && v <= 122 
+                        {
+                            v = v - 96;
+                        }
+                        else if v >= 65 && v <= 90
+                        {
+                            v = v - (65 - 27);
+                        }
+                        
+                        if counter < len
+                        {
+                            a.push(v);
+                        }
+                        else
+                        {
+                            b.push(v);
+                        }
+
+                        counter += 1;
+                    }
+
+                    a.sort();
+                    a.dedup();
+
+                    for w in a
+                    {
+                        let res = b.iter().find(|&&x| x == w);
+                        if res != None 
+                        {
+                            sum += w;                         
+                        }                        
+                    }
+
+                }
+            }
+        }
+    }
+
+   println!("Sum {}", sum);
+}
+
+fn in_poor_elf3_b()
+{
+    let mut sum:u32 = 0;
+   
+    let mut a:Vec<u32> = Vec::new();
+    let mut b:Vec<u32> = Vec::new();
+
+    let mut elf_counter = 0;
+
+    if let Ok(lines) = read_lines("elf3.txt")
+    {
+        for line in lines 
+        {
+            if let Ok(ip) = line
+            {
+                for ch in ip.chars()
+                {                        
+                    let mut v = ch as u32; 
+
+                    if v >= 97 && v <= 122 
+                    {
+                        v = v - 96;
+                    }
+                    else if v >= 65 && v <= 90
+                    {
+                        v = v - (65 - 27);
+                    }
+                    
+                    a.push(v);                        
+                }
+
+                a.sort();
+                a.dedup();
+
+                b.append(&mut a);
+                a.clear();                    
+
+                if elf_counter % 3 == 2 
+                {
+                    b.sort();
+
+                    let mut previous:u32 = 0;
+                    let mut count = 0;
+
+                    for moo in &b 
+                    {
+                        if previous == *moo
+                        {
+                            count += 1;        
+                        }
+                        else
+                        {
+                            count = 0;
+                        }
+
+                        if count >= 2
+                        {
+                            sum += *moo;
+                        }
+
+                        previous = *moo;
+                    }
+
+                    b.clear();
+
+                }
+
+                elf_counter += 1;
+            }
+        }
+    }
+
+   println!("Sum {}", sum);
+}
+
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where P: AsRef<Path> 
 {
@@ -50,5 +191,5 @@ where P: AsRef<Path>
 
 fn main() 
 {
-    in_poor_elf1();
+    in_poor_elf3_b();
 }
